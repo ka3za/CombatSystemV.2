11d7329bcf541e36db15bb52c861295bca36b61a
Mathias-Desktop <mathias67@hotmail.dk>
2016-11-28 14:43:34 +0100

    Added: UI til at skifte klasse Fix? : spiller bevægelse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,9 +13,9 @@ public class Player : Entity {
     private Sprite[] playerSprites2;
     [SerializeField]
     private Sprite[] playerSprites3;
-
+    [SerializeField]
     private Text healthText;
-
+    [SerializeField]
     private Text energySourceText;
 
     private Text moveCostText;
@@ -24,21 +24,66 @@ public class Player : Entity {
 
     private GameObject turnManager;
 
+    [SerializeField]
+    private Dropdown dropDown;
+
+    [SerializeField]
+    private Canvas canvas;
+
     // Use this for initialization
     void Start () {
+        dropDown.onValueChanged.AddListener(delegate
+        {
+            dropDownValueChangedHandler(dropDown);
+        });
+
         CurrentClass = new Tank();
+        UpdateStats();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        MenuKeyHandling(); 
+
+    }
 
     void FixedUpdate()
     {
         RealTimeMovement();
     }
 
+
+    void Destroy()
+    {
+        dropDown.onValueChanged.RemoveAllListeners();
+    }
+
+    private void dropDownValueChangedHandler(Dropdown target)
+    {
+        switch (target.value)
+        {
+            case 0:
+                CurrentClass = new Tank();
+                Debug.Log("Changed to tank");
+                break;
+            case 1:
+                CurrentClass = new Mage();
+                Debug.Log("Changed to mage");
+                break;
+            case 2:
+                CurrentClass = new Hunter();
+                Debug.Log("Changed to hunter");
+                break;
+            default:
+                break;
+        }
+        UpdateStats();
+    }
+
+    public void SetDropdownIndex(int index)
+    {
+        dropDown.value = index;
+    }
 
     public void UpdateStats()
     {
@@ -65,7 +110,7 @@ public class Player : Entity {
 
     private void RealTimeMovement()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.left * CurrentClass.MovementSpeed);
             switch (CurrentClass.TheClassType)
@@ -84,7 +129,48 @@ public class Player : Entity {
             }
 
         }
-        if (Input.GetKey(KeyCode.D))
+
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().AddForce((Vector2.left + Vector2.up) * CurrentClass.MovementSpeed);
+            switch (CurrentClass.TheClassType)
+            {
+                case BaseClass.ClassType.Hunter:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites3[2];
+                    break;
+                case BaseClass.ClassType.Mage:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites2[2];
+                    break;
+                case BaseClass.ClassType.Tank:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites[2];
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().AddForce((Vector2.left + Vector2.down) * CurrentClass.MovementSpeed);
+            switch (CurrentClass.TheClassType)
+            {
+                case BaseClass.ClassType.Hunter:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites3[2];
+                    break;
+                case BaseClass.ClassType.Mage:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites2[2];
+                    break;
+                case BaseClass.ClassType.Tank:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites[2];
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.right * CurrentClass.MovementSpeed);
             switch (CurrentClass.TheClassType)
@@ -103,7 +189,49 @@ public class Player : Entity {
             }
 
         }
-        if (Input.GetKey(KeyCode.W))
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().AddForce((Vector2.right + Vector2.up) * CurrentClass.MovementSpeed);
+            switch (CurrentClass.TheClassType)
+            {
+                case BaseClass.ClassType.Hunter:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites3[1];
+                    break;
+                case BaseClass.ClassType.Mage:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites2[1];
+                    break;
+                case BaseClass.ClassType.Tank:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites[1];
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().AddForce((Vector2.right + Vector2.down) * CurrentClass.MovementSpeed);
+            switch (CurrentClass.TheClassType)
+            {
+                case BaseClass.ClassType.Hunter:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites3[1];
+                    break;
+                case BaseClass.ClassType.Mage:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites2[1];
+                    break;
+                case BaseClass.ClassType.Tank:
+                    GetComponent<SpriteRenderer>().sprite = playerSprites[1];
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * CurrentClass.MovementSpeed);
             switch (CurrentClass.TheClassType)
@@ -122,7 +250,8 @@ public class Player : Entity {
             }
 
         }
-        if (Input.GetKey(KeyCode.S))
+
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.down * CurrentClass.MovementSpeed);
             switch (CurrentClass.TheClassType)
@@ -148,6 +277,14 @@ public class Player : Entity {
         }
 
         playerCam.GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y, -10);
+    }
+
+    private void MenuKeyHandling()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            canvas.enabled = true;
+        }
     }
 
     private void TurnBasedMovement()
